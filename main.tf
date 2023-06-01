3696a4a49a732538331a5079eccc2f7f059043b9
tone_herndon <tonecloudsys@gmail.com>
2023-06-01 09:12:45 -0400

    Modified Dns records

--- main.tf
+++ main.tf
@@ -100,6 +100,7 @@ resource "aws_route53_zone" "tdh-resume-zone" {
 }
 # Creates an ACM Certificate
 resource "aws_acm_certificate" "tdh-resume-certificate" {
+    provider = aws.virginia
     domain_name = var.root_domain_name
     validation_method = "DNS"
     lifecycle {
@@ -113,11 +114,30 @@ records = [tolist(aws_acm_certificate.tdh-resume-certificate.domain_validation_o
 type = tolist(aws_acm_certificate.tdh-resume-certificate.domain_validation_options)[0].resource_record_type
 zone_id = aws_route53_zone.tdh-resume-zone.zone_id
 ttl = 60
+provider = aws.virginia
 }
 
 resource "aws_acm_certificate_validation" "tdh-cert-validate" {
+    provider = aws.virginia
     certificate_arn = aws_acm_certificate.tdh-resume-certificate.arn
     validation_record_fqdns = [aws_route53_record.tdh-cert-dns.fqdn]
+}
+
+#alias record for cloudfront
+resource "aws_route53_record" "cf-record" {
+    depends_on = [
+        aws_cloudfront_distribution.s3_distribution
+    ]
+
+    zone_id = aws_route53_zone.tdh-resume-zone.zone_id
+    name = "tdh-resume.com"
+    type = "A"
+    alias {
+        name = aws_cloudfront_distribution.s3_distribution.domain_name
+        zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+        evaluate_target_health = false
+    }
+    provider = aws.virginia
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
